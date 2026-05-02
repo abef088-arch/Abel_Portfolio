56355e496579bc5c149670c009d243a975b28e43
Abel Farajov <brawl20937stpig@gmail.com>
2026-05-02 16:39:38 +0400

    Sayt temizlendi ve duzelisler edildi

--- script.js
+++ script.js
@@ -15,22 +15,4 @@ fetch('footer.html')
   .then(data => {
     document.getElementById('footer').innerHTML = data;
   })
-
-// script.js - Modular JavaScript
-
-// Load navigation
-fetch('nav.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('nav').innerHTML = data;
-  })
-  .catch(error => console.error('Error loading navigation:', error));
-
-// Load footer
-fetch('footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
-  })
-
   .catch(error => console.error('Error loading footer:', error));
